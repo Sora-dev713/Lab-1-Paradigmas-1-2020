5d4a91aad58438ca4cc8b3c26405202f46682bfa
Bastian Onetto <bastian.onetto@usach.cl>
2020-05-22 14:07:46 -0400

    Fn Auxiiares y TDAs
    se rehacen los cambios de 2 commits perdidos y se agregan funciones auxiliares

--- gitSim.rkt
+++ gitSim.rkt
@@ -3,17 +3,33 @@
 ;TDA Archivos:
 ;Representación: "Nombre_Archivo.extensión"
 ;Constructor:
-(define (archivo Nombre extension)
-  (if(and (string? Nombre)(string? extension)(<= (string-length extension) 3))
-     (string-join (list Nombre extension) ".")
+(define (archivo Nombre contenido)
+  (if(and (string? Nombre)(string? contenido)(<= (string-length contenido) 0))
+     (list Nombre contenido)
      '())
   )
 
 ;pertenencia:
 (define (archivo? A)
-  (if(and(string? A)(<= (string-length (cadr (string-split A "."))) 3))
-     #t
-     #f))
+  (and (list? A)
+       (not(null? A))
+       (string? (car A))
+       (> (string-length (car (string-split A "."))) 0)
+       (> (string-length (cadr (string-split A "."))) 0)
+       (> (string-length (cadr A)) 0)))
+
+;Selectores:
+(define (getName A)
+  (if (archivo? A)
+      (car A)
+      '()))
+
+(define (getContent A)
+  (if (archivo? A)
+      (cadr A)
+      '()))
+;----------------0
+     
 ;---------------------------------
 ;Auxiliar: Lista de archivos
 (define (listArch? L)
@@ -37,4 +53,73 @@
      #t
      #f))
 ;----------------------------------------------------
-;TDA Zona
+;TDA Workspace
+;Represetacion: ["Workspace", Lista_Archivos]
+;costructor
+;Dominio: Archivo/S
+;Recorrido: Workspace
+(define (createWorskpace A)
+  (if (listArch? A)
+      (list "Workspace" A)
+      (if (archivo? A)
+          (list "Workspace" (cons A '()))
+          '())))
+
+;Funcion de pertenencia
+;Domio: Workspace
+;Recorrido: Boolean
+(define (workspace? W)
+  (and(list? W)(not(null? W))(eq? "Workspace" (car W))(listArch? (cadr W))(null? (cddr W))))
+
+
+;----------------------------------------------------------------------------
+;TDA Index
+;Represetacion: ["Index", Lista_Archivos]
+;costructor
+;Dominio: Archivo/S
+;Recorrido: Index
+(define (createIndex A)
+  (if (listArch? A)
+      (list "Index" A)
+      (if (archivo? A)
+          (list "Index" (cons A '()))
+          '())))
+
+;Funcion de pertenencia
+;Domio: Index
+;Recorrido:
+(define (index? I)
+  (and(list? I)(not(null? I))(eq? "Index" (car I))(listArch? (cadr I))(null? (cddr I))))
+
+;---------------------------Funciones Auxiliares-------------------------------
+;Check Names
+;Funcion que comprueba que un archivo ya este en la lista
+(define (checkName N L)
+  (if (null? (cdr L))
+      (equal? N (getName (car L)))
+      (or (equal? N (getName (car L)))(checkName N (cdr L)))
+
+
+
+;MergeFiles
+;funcion que mezcla los archivos desde 2 listas (A|B), privilegiando los de A
+(define (mergeFiles A B)
+  (if (and(listArch? A)(listArch? B))
+      (unifyFiles A B)
+      '()))
+;UnifyFiles
+;Funcion recursiva que unifica los archivos en una lista
+(define (unifyFiles A B C)
+  (if (or (null? B)(null? A))
+      A
+      (if (checkName (car A) B)
+          (unifyFiles (cdr A) (cons B A)))
+
+
+;----------------------------------------------------------------------
+;TDA Repository
+;Representacion: ["NombreReposiorio", Lista_Commits,Lista_Archivos]
+;constructor:
+;Dominio: Lista_Commits/Commit
+;Recorrido: Repositorio
+
